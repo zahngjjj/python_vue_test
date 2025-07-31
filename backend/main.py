650363902e82f758331634dce9b1851131ca593b
zhangjinjian <1016417093@qq.com>
2025-07-31 16:01:18 +0800

    feat: add new

--- backend/main.py
+++ backend/main.py
@@ -29,4 +29,6 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
